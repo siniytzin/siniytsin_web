ce66182e44cc6d91d2a99a2a92213cdd3445b21b
siniytsin <sashasinitzin@gmail.com>
2022-07-06 20:55:42 +0300

    added slider

--- js/main.js
+++ js/main.js
@@ -3,3 +3,32 @@ $('.accordion').accordion({
 	header: '.accordion-header',
   active: 0
 });
+
+$('.center').slick({
+  centerMode: true,
+  // autoplay: true,
+	dots: true,
+	arrows:false,
+  centerPadding: '20px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 1190,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '30px',
+        slidesToShow: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '10px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
